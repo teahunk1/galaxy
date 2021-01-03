93824d65ca0fc93c0db070c9dd40da0ff89d3caa
teahunk1 <76608306+teahunk1@users.noreply.github.com>
2021-01-03 20:10:00 +0900

    Add files via upload
    반복문 while for

---
+++ day3-1.py
@@ -0,0 +1,21 @@
+#a=int(input())
+
+#b=1
+#print("입력한 수는  "+str(a))
+
+#while b<10:
+#   print(str(a)+"x"+str(b)+"="+str(a*b))
+#   b=b+1
+
+a=1
+b=1
+
+while b<5:
+    a=1
+    while a<5:
+        if a>2:
+            break
+        print(a,b)
+        a+=1
+    b+=1
+        
